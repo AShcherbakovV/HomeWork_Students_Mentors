647de1a81421b23871d1b8c7a72c1284bc67fb04
A.Shcherbakov <andreyka13327@gmail.com>
2024-02-28 21:52:07 +0300

    Added magic method __str__()

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 class Student:
     def __init__(self, name, surname, gender):
-
-
         self.name = name
         self.surname = surname
         self.gender = gender
@@ -9,6 +7,14 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def __str__(self):
+        return (f'Имя: {self.name}\n'
+                f'Фамилия: {self.surname}\n'
+                f'Средняя оценка за лекции: {self.mid_grade()}\n'
+                f'Курсы в процессе изучения: {", ".join(self.courses_in_progress)}\n'
+                f'Завершенные курсы: {", ".join(self.finished_courses)}')
+
+    
     def grade_lector(self, lector, course, grade):
         if isinstance(lector, Lecturer) and course in lector.courses_attached and course in self.courses_in_progress:
             if course in lector.grades:
@@ -17,6 +23,17 @@ class Student:
                 lector.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+    def mid_grade(self):
+        mid = 0.0
+        count = 0
+
+        for course in self.grades.values():
+            count += len(course)
+            for grade in course:
+                mid += grade
+
+        return mid / count
 
 
 class Mentor:
@@ -34,14 +51,36 @@ class Mentor:
         else:
             return 'Ошибка'
 
+
 class Lecturer(Mentor):
     def __init__(self, name, surname):
         Mentor.__init__(self, name, surname)
         self.grades={}
 
+    def __str__(self):
+        mid = 0.0
+        count = 0
+
+        for course in self.grades.values():
+            count += len(course)
+            for grade in course:
+                mid += grade
+
+        mid = mid/count
+
+        return (f'Имя: {self.name}\n'
+                f'Фамилия: {self.surname}\n'
+                f'Средняя оценка за лекции: {mid}\n')
+
+
+
 class Reviewer(Mentor):
     def __init__(self, name, surname):
         Mentor.__init__(self, name, surname)
+
+    def __str__(self):
+        return (f'Имя: {self.name}\n'
+                f'Фамилия: {self.surname}')
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
@@ -53,8 +92,11 @@ lecturer_1 = Lecturer('Tomas', 'Henderson')
 lecturer_2 = Lecturer('James', 'Bold')
 lecturer_1.courses_attached += ['Python']
 
+lecturer_1.rate_hw(best_student, 'Python', 10.0)
+lecturer_2.rate_hw(best_student, 'Python', 7.0)
+
 best_student.grade_lector(lecturer_1, 'Python', 9.0)
 best_student_2.grade_lector(lecturer_1, 'Python', 7.0)
 
-print(lecturer_1.grades)
-print(best_student.grades)
+print(lecturer_1)
+print(best_student)
