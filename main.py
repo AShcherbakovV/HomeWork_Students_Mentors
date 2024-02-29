ed46eb4959e49489988ff5e2d517d8b5340a2c13
A.Shcherbakov <andreyka13327@gmail.com>
2024-02-29 17:13:50 +0300

    added mid grades lecturers & students

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-#Инициализация класса Student
+#Объявление класса student
 class Student:
-    def __init__(self, name, surname, gender):
+    def __init__(self, name, surname, gender): #Инициализация класса student
         self.name = name
         self.surname = surname
         self.gender = gender
@@ -15,13 +15,13 @@ class Student:
                 f'Курсы в процессе изучения: {", ".join(self.courses_in_progress)}\n'
                 f'Завершенные курсы: {", ".join(self.finished_courses)}\n')
 
-    def __eq__(self, other):  #Переопределение метода сравнения
+    def __eq__(self, other):  #Переопределение метода сравнения __eq__
         return self.mid_grade() == other.mid_grade()
 
-    def __lt__(self, other):  #
+    def __lt__(self, other):  #Переопределение метода сравнения __lt__
         return self.mid_grade() < other.mid_grade()
 
-    def grade_lector(self, lector, course, grade):  #
+    def grade_lector(self, lector, course, grade):  #Метод добавляющий возможность оценки лектора за курс
         if isinstance(lector, Lecturer) and course in lector.courses_attached and course in self.courses_in_progress:
             if course in lector.grades:
                 lector.grades[course] += [grade]
@@ -30,7 +30,7 @@ class Student:
         else:
             return 'Ошибка'
 
-    def mid_grade(self):  #
+    def mid_grade(self):  #Метод возвращающий средний балла студента
         mid = 0.0
         count = 0
 
@@ -39,15 +39,19 @@ class Student:
             for grade in course:
                 mid += grade
 
-        return mid / count
+        if count != 0:
+            return mid / count
+        else:
+            return 0.0
 
+#Объявление родительского класса Mentor
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
 
-    def rate_hw(self, student, course, grade):
+    def rate_hw(self, student, course, grade): #Метод добавляющий возможность выставления оценки студенту
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
                 student.grades[course] += [grade]
@@ -56,20 +60,24 @@ class Mentor:
         else:
             return 'Ошибка'
 
+#Объявление дочернего класса Lecturer класса Mentor
 class Lecturer(Mentor):
     def __init__(self, name, surname):
         Mentor.__init__(self, name, surname)
-        self.grades={}
+        self.grades={} #Добавление параметра grades для хранения оценок от студентов
 
-    def __str__(self):
+    def __str__(self): #Переопределение магического метода __str__
         return (f'Имя: {self.name}\n'
                 f'Фамилия: {self.surname}\n'
                 f'Средняя оценка за лекции: {self.mid_grade()}\n')
 
-    def __eq__(self, other):
+    def __eq__(self, other): #Переопределение метода сравнения __eq__ для сравнения преподавтелей по среднему баллу
         return self.mid_grade() == other.mid_grade()
 
-    def mid_grade(self):
+    def __lt__(self, other):  #Переопределение метода сравнения __lt__ для сравнения преподавтелей по среднему баллу
+        return self.mid_grade() < other.mid_grade()
+
+    def mid_grade(self): #Метод возвращающий средний балла лектора
         mid = 0.0
         count = 0
 
@@ -77,45 +85,81 @@ class Lecturer(Mentor):
             count += len(course)
             for grade in course:
                 mid += grade
+        if count != 0:
+            return mid / count
+        else:
+            return 0.0
 
-        return mid / count
-
-class Reviewer(Mentor):
+class Reviewer(Mentor): #Объявление класса Reviewer
     def __init__(self, name, surname):
         Mentor.__init__(self, name, surname)
 
-    def __str__(self):
+    def __str__(self): #Переопределение магического метода __str__
         return (f'Имя: {self.name}\n'
                 f'Фамилия: {self.surname}\n')
 
+def lectors_mid_grade(lectors, course):
+    mid = 0.0
+    count = 0
+    for lector in lectors:
+        count += len(lector.grades.get(course))
+        for grade in lector.grades.get(course):
+            mid += grade
+
+    return mid/count
+
+def students_mid_grade(students, course):
+    mid = 0.0
+    count = 0
+    for student in students:
+        count += len(student.grades.get(course))
+        for grade in student.grades.get(course):
+            mid += grade
+
+    return mid/count
+
+
 best_student = Student('Ruoy', 'Eman', 'your_gender')
-not_best_student = Student('Ruoy', 'Eman', 'your_gender')
+not_best_student = Student('Roy', 'Harper', 'your_gender')
 
 best_student.courses_in_progress += ['Python']
+best_student.finished_courses += ['Введение в программирование']
 not_best_student.courses_in_progress += ['Python']
-
-best_student_2 = Student('Ruoy', 'Eman', 'your_gender')
-best_student_2.courses_in_progress += ['Python']
 
 lecturer_1 = Lecturer('Tomas', 'Henderson')
 lecturer_2 = Lecturer('James', 'Bold')
+
 lecturer_1.courses_attached += ['Python']
+lecturer_1.courses_attached += ['Git']
+
+lecturer_2.courses_attached += ['Python']
 
 lecturer_1.rate_hw(best_student, 'Python', 10.0)
 lecturer_1.rate_hw(best_student, 'Python', 7.0)
 
-lecturer_1.rate_hw(best_student_2, 'Python', 6.0)
-lecturer_1.rate_hw(best_student_2, 'Python', 7.0)
+lecturer_1.rate_hw(not_best_student, 'Python', 6.0)
+lecturer_2.rate_hw(not_best_student, 'Python', 7.0)
 
 best_student.grade_lector(lecturer_1, 'Python', 9.0)
-best_student_2.grade_lector(lecturer_1, 'Python', 7.0)
+not_best_student.grade_lector(lecturer_1, 'Git', 7.0)
+not_best_student.grade_lector(lecturer_2, 'Python', 8.4)
 
-reviewer_1 = Reviewer('Gignger', 'Bgread')
+reviewer_1 = Reviewer('Gignger', 'Bread')
+reviewer_2 = Reviewer('Bruce', 'Branner')
 
 print(reviewer_1)
 print(lecturer_1)
+print(lecturer_2)
 print(best_student)
+print(not_best_student)
+print(reviewer_1)
 
-print(best_student > best_student_2)
-print(best_student < best_student_2)
-print(best_student == best_student_2)
+print(best_student > not_best_student)
+print(best_student < not_best_student)
+print(best_student == not_best_student)
+
+print(lecturer_1 == lecturer_2)
+print(lecturer_1 > lecturer_2)
+
+print(lectors_mid_grade([lecturer_1, lecturer_2], 'Python'))
+print(lectors_mid_grade([best_student, not_best_student], 'Python'))
