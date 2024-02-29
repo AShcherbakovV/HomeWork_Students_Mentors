7e9fff257796087b347471c1e543935ff2bcfc4f
A.Shcherbakov <andreyka13327@gmail.com>
2024-02-29 13:57:37 +0300

    Added omparison methods in class

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#Инициализация класса Student
 class Student:
     def __init__(self, name, surname, gender):
         self.name = name
@@ -7,15 +8,20 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
-    def __str__(self):
+    def __str__(self):   #Переопределение магического метода __str__
         return (f'Имя: {self.name}\n'
                 f'Фамилия: {self.surname}\n'
                 f'Средняя оценка за лекции: {self.mid_grade()}\n'
                 f'Курсы в процессе изучения: {", ".join(self.courses_in_progress)}\n'
-                f'Завершенные курсы: {", ".join(self.finished_courses)}')
+                f'Завершенные курсы: {", ".join(self.finished_courses)}\n')
 
-    
-    def grade_lector(self, lector, course, grade):
+    def __eq__(self, other):  #Переопределение метода сравнения
+        return self.mid_grade() == other.mid_grade()
+
+    def __lt__(self, other):  #
+        return self.mid_grade() < other.mid_grade()
+
+    def grade_lector(self, lector, course, grade):  #
         if isinstance(lector, Lecturer) and course in lector.courses_attached and course in self.courses_in_progress:
             if course in lector.grades:
                 lector.grades[course] += [grade]
@@ -24,7 +30,7 @@ class Student:
         else:
             return 'Ошибка'
 
-    def mid_grade(self):
+    def mid_grade(self):  #
         mid = 0.0
         count = 0
 
@@ -34,7 +40,6 @@ class Student:
                 mid += grade
 
         return mid / count
-
 
 class Mentor:
     def __init__(self, name, surname):
@@ -51,13 +56,20 @@ class Mentor:
         else:
             return 'Ошибка'
 
-
 class Lecturer(Mentor):
     def __init__(self, name, surname):
         Mentor.__init__(self, name, surname)
         self.grades={}
 
     def __str__(self):
+        return (f'Имя: {self.name}\n'
+                f'Фамилия: {self.surname}\n'
+                f'Средняя оценка за лекции: {self.mid_grade()}\n')
+
+    def __eq__(self, other):
+        return self.mid_grade() == other.mid_grade()
+
+    def mid_grade(self):
         mid = 0.0
         count = 0
 
@@ -66,13 +78,7 @@ class Lecturer(Mentor):
             for grade in course:
                 mid += grade
 
-        mid = mid/count
-
-        return (f'Имя: {self.name}\n'
-                f'Фамилия: {self.surname}\n'
-                f'Средняя оценка за лекции: {mid}\n')
-
-
+        return mid / count
 
 class Reviewer(Mentor):
     def __init__(self, name, surname):
@@ -80,10 +86,13 @@ class Reviewer(Mentor):
 
     def __str__(self):
         return (f'Имя: {self.name}\n'
-                f'Фамилия: {self.surname}')
+                f'Фамилия: {self.surname}\n')
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
+not_best_student = Student('Ruoy', 'Eman', 'your_gender')
+
 best_student.courses_in_progress += ['Python']
+not_best_student.courses_in_progress += ['Python']
 
 best_student_2 = Student('Ruoy', 'Eman', 'your_gender')
 best_student_2.courses_in_progress += ['Python']
@@ -93,10 +102,20 @@ lecturer_2 = Lecturer('James', 'Bold')
 lecturer_1.courses_attached += ['Python']
 
 lecturer_1.rate_hw(best_student, 'Python', 10.0)
-lecturer_2.rate_hw(best_student, 'Python', 7.0)
+lecturer_1.rate_hw(best_student, 'Python', 7.0)
+
+lecturer_1.rate_hw(best_student_2, 'Python', 6.0)
+lecturer_1.rate_hw(best_student_2, 'Python', 7.0)
 
 best_student.grade_lector(lecturer_1, 'Python', 9.0)
 best_student_2.grade_lector(lecturer_1, 'Python', 7.0)
 
+reviewer_1 = Reviewer('Gignger', 'Bgread')
+
+print(reviewer_1)
 print(lecturer_1)
 print(best_student)
+
+print(best_student > best_student_2)
+print(best_student < best_student_2)
+print(best_student == best_student_2)
