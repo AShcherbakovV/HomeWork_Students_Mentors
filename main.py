8ca5085a61b388c72b6211ff4d7eb60f8e3c5ee7
A.Shcherbakov <andreyka13327@gmail.com>
2024-03-04 12:50:57 +0300

    Final version

--- main.py
+++ main.py
@@ -33,7 +33,6 @@ class Student:
     def mid_grade(self):  #Метод возвращающий средний балла студента
         mid = 0.0
         count = 0
-
         for course in self.grades.values():
             count += len(course)
             for grade in course:
@@ -64,9 +63,9 @@ class Mentor:
 class Lecturer(Mentor):
     def __init__(self, name, surname):
         Mentor.__init__(self, name, surname)
-        self.grades={} #Добавление параметра grades для хранения оценок от студентов
+        self.grades={}            #Добавление параметра grades для хранения оценок от студентов
 
-    def __str__(self): #Переопределение магического метода __str__
+    def __str__(self):       #Переопределение магического метода __str__
         return (f'Имя: {self.name}\n'
                 f'Фамилия: {self.surname}\n'
                 f'Средняя оценка за лекции: {self.mid_grade()}\n')
@@ -98,6 +97,7 @@ class Reviewer(Mentor): #Объявление класса Reviewer
         return (f'Имя: {self.name}\n'
                 f'Фамилия: {self.surname}\n')
 
+#Функция возвращает средний балл лекторов по указанному курсу из списка полученного на вход
 def lectors_mid_grade(lectors, course):
     mid = 0.0
     count = 0
@@ -108,6 +108,7 @@ def lectors_mid_grade(lectors, course):
 
     return mid/count
 
+#Функция возвращает средний балл студентов по указанному курсу из списка полученного на вход
 def students_mid_grade(students, course):
     mid = 0.0
     count = 0
@@ -118,7 +119,7 @@ def students_mid_grade(students, course):
 
     return mid/count
 
-
+#Создаем два экзэмпляра класса student
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 not_best_student = Student('Roy', 'Harper', 'your_gender')
 
@@ -126,40 +127,46 @@ best_student.courses_in_progress += ['Python']
 best_student.finished_courses += ['Введение в программирование']
 not_best_student.courses_in_progress += ['Python']
 
+#Создаем два экзэмпляра класса Lecturer
 lecturer_1 = Lecturer('Tomas', 'Henderson')
 lecturer_2 = Lecturer('James', 'Bold')
-
+#Добавляем активные курсы лекторам 1 и 2
 lecturer_1.courses_attached += ['Python']
 lecturer_1.courses_attached += ['Git']
-
 lecturer_2.courses_attached += ['Python']
 
-lecturer_1.rate_hw(best_student, 'Python', 10.0)
-lecturer_1.rate_hw(best_student, 'Python', 7.0)
+#Создаем два экзэмпляра класса Reviewer
+reviewer_1 = Reviewer('Gignger', 'Bread')
+reviewer_2 = Reviewer('Bruce', 'Branner')
+#Добавляем активные курсы проверяющим 1 и 2
+reviewer_1.courses_attached += ['Python']
+reviewer_2.courses_attached += ['Git']
 
-lecturer_1.rate_hw(not_best_student, 'Python', 6.0)
-lecturer_2.rate_hw(not_best_student, 'Python', 7.0)
+#Проверяющий 1 выставляет оценку студентам по курсу Python
+reviewer_1.rate_hw(best_student, 'Python', 10.0)
+reviewer_1.rate_hw(best_student, 'Python', 7.0)
 
+reviewer_1.rate_hw(not_best_student, 'Python', 6.0)
+reviewer_1.rate_hw(not_best_student, 'Python', 7.0)
+#Студенты выставляют оценки лекторам
 best_student.grade_lector(lecturer_1, 'Python', 9.0)
 not_best_student.grade_lector(lecturer_1, 'Git', 7.0)
 not_best_student.grade_lector(lecturer_2, 'Python', 8.4)
-
-reviewer_1 = Reviewer('Gignger', 'Bread')
-reviewer_2 = Reviewer('Bruce', 'Branner')
-
-print(reviewer_1)
+#Вывод информации о преподавателях и студентах
 print(lecturer_1)
 print(lecturer_2)
 print(best_student)
 print(not_best_student)
 print(reviewer_1)
+print(reviewer_2)
+#Вывод результата сравнения студентов
+print(f'Резултат сравнения студентов по среднему баллу (>) {best_student > not_best_student}')
+print(f'Резултат сравнения студентов по среднему баллу (<) {best_student < not_best_student}')
+print(f'Резултат сравнения студентов по среднему баллу (==) {best_student == not_best_student}')
+#Вывод результата сравнения лекторов
+print(f'Резултат сравнения студентов по среднему баллу (==) {lecturer_1 == lecturer_2}')
+print(f'Резултат сравнения студентов по среднему баллу (>) {lecturer_1 > lecturer_2}')
+#Вывод среднего балла преподавателей и студентов по курсу
+print('Средняя оценка лекторов по курсу Python: ' + str(lectors_mid_grade([lecturer_1, lecturer_2], 'Python')))
+print('Средняя оценка студентов по курсу Python: ' + str(students_mid_grade([best_student, not_best_student], 'Python')))
 
-print(best_student > not_best_student)
-print(best_student < not_best_student)
-print(best_student == not_best_student)
-
-print(lecturer_1 == lecturer_2)
-print(lecturer_1 > lecturer_2)
-
-print(lectors_mid_grade([lecturer_1, lecturer_2], 'Python'))
-print(lectors_mid_grade([best_student, not_best_student], 'Python'))
